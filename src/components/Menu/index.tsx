2ae2096dc29db4d37efb402710df9dc106a7f82d
clarawm <mariaclaramiguel.c@gmail.com>
2022-08-12 10:01:13 -0300

    oi :)

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logobook from "assets/img/logobook.png"
+import * as S from "./styles";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <S.Cabecalho>
+      <picture>
+        <Link to="/">
+          <img src={logobook} alt="Imagens principais" />
+        </Link>
+      </picture>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/cadastrar">Cadastrar</Link>
+          </li>
+        </ul>
+      </nav>
+    </S.Cabecalho>
+  );
+};
+
+export default Menu;
---
+++ src/services/dados.ts
@@ -0,0 +1,15 @@
+const dados = [
+  {
+    id: 1,
+    nome: "Lázaro",
+    data: "27/04/2022 10:08:00",
+    titulo: "Blusa",
+    mensagem: "Tenho uma blusa para doar, alguém interessa?",
+    topico: [{
+      id: 1,
+      item: "Entrar"
+    },]
+  },
+]
+
+export default dados
