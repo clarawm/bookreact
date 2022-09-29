ed7905004d2ca22856df62f3b8ae272ae8f799a9
clarawm <mariaclaramiguel.c@gmail.com>
2022-09-29 17:05:07 -0300

    Subiu sim :)

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import logobook from "assets/img/logobook.png"
+import { GrLogout } from "react-icons/gr";
 import * as S from "./styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "hooks/authHook";
 
 const Menu = () => {
+  const { user, signOut } = useAuth()
+  const navigate = useNavigate();
+  async function logout() {
+    await signOut()
+    navigate('/login')
+  }
   return (
     <S.Cabecalho>
       <picture>
@@ -12,17 +20,27 @@ const Menu = () => {
         </Link>
       </picture>
       <nav>
-        <ul>
-          <li>
-          <Link to="/cadastrar">Cadastro</Link>
-          </li>
-          <li>
-           <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        {
+          user ? (
+            <ul>
+              <li>
+
+                <button onClick={logout}>{user.name} <GrLogout /></button>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <Link to="/cadastrar">Cadastro</Link>
+              </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+          )
+        }
       </nav>
-    </S.Cabecalho>
+    </S.Cabecalho >
   );
 };
-
 export default Menu;
