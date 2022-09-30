500ed3ca0ea14460a3993afe13c14b94586387c1
clarawm <mariaclaramiguel.c@gmail.com>
2022-09-30 10:26:51 -0300

    Metade da metade pronta

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,14 +1,38 @@
 import styled from "styled-components"
 import { colors } from "styles/GlobalStyle"
 
-export const Botao = styled.button`
-  color: ${colors.black};
+const colorVariations = {
+  default: css`
+    background-color: ${colors.third};
+    outline: 0.2rem outset ${colors.third};
+  `,
+  add: css`
+    background-color: ${colors.primary};
+    outline: 0.2rem outset ${colors.primary};
+  `,
+  edit: css`
+    background-color: ${colors.success};
+    outline: 0.2rem outset ${colors.success};
+  `,
+  remove: css`
+    background-color: ${colors.danger};
+    outline: 0.2rem outset ${colors.danger};
+  `
+}
+
+export const Botao = styled.button<ButtonProps>`
+  color: ${colors.white};
   font-weight: bold;
-  font-size: 1rem;
-  background-color: ${colors.primary};
+  background-color: ${colors.third};
   padding: 0.6rem;
   border: 0;
   border-radius: 0.6rem;
   width: 5rem;
   margin-left: 0.5rem;
+  cursor: pointer;
+  svg {
+    width: 2rem;
+    height: 2rem;
+  }
+  ${(props) => colorVariations[props.bgColor || "default"]}
 `
